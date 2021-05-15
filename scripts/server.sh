be64cca53e3d1a3a61b3e2517e712812796212b1
ZenithalHourlyRate <i@zenithal.me>
2021-05-16 01:51:21 +0800

    Change mode (+x) for server.sh and Add variable

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 kindle=kindle
+src=~/.config/i3/grafana.png
 
 p=$(dirname "$0")
 
-convert ~/.config/i3/grafana.png -rotate 90 $p/grafana.png
+convert $src -rotate 90 $p/grafana.png
 $p/png8.sh $p/grafana.png $p/_grafana.png
 scp -o ConnectTimeout=5 -o BatchMode=yes -o StrictHostKeyChecking=no $p/_grafana.png $kindle:/tmp/screen.png > /dev/null
 ssh -o ConnectTimeout=5 -o BatchMode=yes -o StrictHostKeyChecking=no $kindle /mnt/us/extensions/screen/bin/screen.sh update > /dev/null
